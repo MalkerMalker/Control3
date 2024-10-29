4330a26d0b38c25212e9d59446a31ea05bee387d
MalkerMalker <rodrigo.manriquez.bastias@gmail.com>
2024-10-29 15:54:07 -0300

    Update tarea.py

--- tarea.py
+++ tarea.py
@@ -2,5 +2,5 @@ import streamlit as st
 st.title("Registro civil")
 st.write("Pagina gubernamental para la inscripción de datos civiles. Esta página se encuentra sujeta bajo la ley N. 129 del articulo 34.")
 st.sidebar.image("descarga.png")
-st.button("Haz click aqui para ver el articulo" )
-st.markdown("[Haz clic aquí para visitar OpenAI](https://www.openai.com)")
+if st.button("Haz click aqui para ver el articulo" )
+    webbrowser.open("https://www.dipres.gob.cl/598/articles-51683_Otrasleyes_ley19628.pdf")
