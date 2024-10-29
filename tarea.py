830a67a319dd77f5d628468d39f5ec6e34e764be
MalkerMalker <rodrigo.manriquez.bastias@gmail.com>
2024-10-29 15:46:47 -0300

    Update tarea.py

--- tarea.py
+++ tarea.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.title("Registro civil")
 st.header("Pagina gubernamental para la inscripción de datos civiles. Esta página se encuentra sujeta bajo la ley N. 129 del articulo 34.")
-st.sidebar.title("Ingrese su rut")
+st.sidebar.title("Registro")
