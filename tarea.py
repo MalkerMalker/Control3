90b0ef539c1ba244dcfc80d4f8188b235a9219e1
MalkerMalker <rodrigo.manriquez.bastias@gmail.com>
2024-10-29 15:42:51 -0300

    Update tarea.py

--- tarea.py
+++ tarea.py
@@ -2,3 +2,4 @@ import streamlit as st
 st.title("Hola mundo")
 st.header("En esta pagina veremos todo lo relacionado con hola mundo")
 st.subheader("Texto simple")
+st.markdown("Texto en formato Markdown")
