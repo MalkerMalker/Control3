8a6e48f482f8b1dd1ea8de61664486da53aae436
MalkerMalker <rodrigo.manriquez.bastias@gmail.com>
2024-10-29 16:02:55 -0300

    Update tarea.py

--- tarea.py
+++ tarea.py
@@ -2,9 +2,9 @@ import streamlit as st
 import webbrowser
 st.title("Registro civil")
 
-st.write("El tratamiento de los datos de carácter personal en registros o bancos de datos por organismos públicos o por particulares se sujetará a las disposiciones 
+st.write("""El tratamiento de los datos de carácter personal en registros o bancos de datos por organismos públicos o por particulares se sujetará a las disposiciones 
 de esta ley, con excepción del que se efectúe en ejercicio de las libertades de emitir opinión y de informar, el que se regulará por la ley a que se refiere el artículo 
-19, Nº 12, de la Constitución Política.")
+19, Nº 12, de la Constitución Política.""")
 
 ingreso = st.sidebar.text_input("Ingrese su rut")
 
