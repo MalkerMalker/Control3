0b2ebe9fcb6dcd47e950feb2721485307eb204a2
MalkerMalker <rodrigo.manriquez.bastias@gmail.com>
2024-10-29 16:24:02 -0300

    Update tarea.py

--- tarea.py
+++ tarea.py
@@ -13,6 +13,6 @@ if st.button("Haz click aqui para ver el articulo"):
 ingreso = st.sidebar.text_input("Ingrese su rut")
 contraseña = st.sidebar.text_input("Ingrese su clave única")
 
-if st.sidebar.button("ads"):
+if st.sidebar.button("Click para ver el promedio de registro"):
     st.write("Hola")
     
