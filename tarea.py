545b3c4def75786a0eafd11e7d0a378ce8188c43
MalkerMalker <rodrigo.manriquez.bastias@gmail.com>
2024-10-29 16:03:18 -0300

    Update tarea.py

--- tarea.py
+++ tarea.py
@@ -6,8 +6,8 @@ st.write("""El tratamiento de los datos de carácter personal en registros o ban
 de esta ley, con excepción del que se efectúe en ejercicio de las libertades de emitir opinión y de informar, el que se regulará por la ley a que se refiere el artículo 
 19, Nº 12, de la Constitución Política.""")
 
-ingreso = st.sidebar.text_input("Ingrese su rut")
-
 st.sidebar.image("descarga.png")
 if st.button("Haz click aqui para ver el articulo"):
     webbrowser.open("https://www.dipres.gob.cl/598/articles-51683_Otrasleyes_ley19628.pdf")
+
+ingreso = st.sidebar.text_input("Ingrese su rut")
